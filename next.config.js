f9f615b69cd74f841ec5d37844b3cfca511f4746
Paul Popus <paul@nouance.io>
2024-07-08 17:06:52 -0400

    update next config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,16 @@
-/** @type {import('next').NextConfig} */
 import { withPayload } from '@payloadcms/next/withPayload'
 
 import redirects from './redirects.js'
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', process.env.NEXT_PUBLIC_SERVER_URL]
+    remotePatterns: ['localhost', process.env.NEXT_PUBLIC_SERVER_URL]
       .filter(Boolean)
       .map((url) => url.replace(/https?:\/\//, '')),
   },
   reactStrictMode: true,
   redirects,
-  swcMinify: true,
 }
 
 export default withPayload(nextConfig)
